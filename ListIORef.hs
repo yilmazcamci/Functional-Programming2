c6ad2fb4d0960b098440fc7962bc046c1935ca63
MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
2023-12-23 12:51:56 +0100

    Update ListIORef.hs

--- ListIORef.hs
+++ ListIORef.hs
@@ -16,7 +16,32 @@ modifyIORef :: IORef a -> (a -> a) -> IO ()
 
 -}
 
-ListIORef = IORef ListRef
-ListRef   = Nil | Cons LisIORef -- see what we're doing here ;)
+copy : : IORef a -> IORef a -> IO ()
+copy x y = do 
+    val <- readIORef y
+    writeIORef x val
+    
+swap : : IORef a -> IORef a -> IO ()
+swap x y = do 
+    a <- readIORef x
+    b <- readIORef y
+    writeIORef x b
+    writeIORef y a
+
+ListRef a = IORef (List a)
+List a  = Nil | Cons (ListRef a)-- see what we're doing here ;)
 
 
+last :: ListRef a -> IO (ListRef a) 
+-- returns a reference to the last element of the list
+last ref = do
+    val <- readIORef ref
+    case val of
+        Nil -> pure ref
+        Cons x ys -> do
+            
+
+append :: ListRef a -> ListRef a -> IO ()
+append ref1 ref2 = do
+    val1 <- readIORef ref1
+    
